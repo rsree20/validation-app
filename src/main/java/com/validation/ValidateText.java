603831d71d2a4ccac9c5cb521f437e16688f6f9e
SREE <SREE@DESKTOP-OHJSHRN>
2018-12-04 21:07:46 +0530

    master commit

---
+++ src/main/java/com/validation/ValidateText.java
@@ -0,0 +1,17 @@
+package com.validation;
+
+public class ValidateText {
+
+	public void testCheck() {
+		
+		System.out.println("flow inside valid-app");
+		
+		
+		System.out.println("hii");
+		
+		System.out.println("test flow");
+		System.out.println("test flow checked");
+		System.out.println("hello");
+		System.out.println("hello user");
+	}
+}
